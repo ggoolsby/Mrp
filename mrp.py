6b9f856f7adebdbd63bf39e8a8da90d027575471
ggoolsby <ggoolsby@middlebury.edu>
2018-08-07 07:49:05 -0400

    initial commit

---
+++ mrp.py
@@ -0,0 +1,10 @@
+"""
+@author: graygoolsby
+08/07/2018
+Create lite Manufacturing Resource Planning system that outputs production runs by cross-referencing
+inventory levels, sales orders, and WCL output
+"""
+
+def mrp(day, month, year):
+    
+    
